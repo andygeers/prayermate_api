0f3da56736117e392fbf742b9d1e7b3f66c1586c
Ed Bennigsen <ed@prayermate.net>
2017-11-24 16:01:37 +0000

    Merged in code from PrayerMate Send API classes. api_key now a variable passed into the API on creation

--- lib/prayermate_api.rb
+++ lib/prayermate_api.rb
@@ -1,36 +1,89 @@
 require "prayermate_api/version"
 require "httparty"
+require "securerandom"
 
 module PrayerMateApi
 
-  # TODO: replace with environment variable
-  API_KEY = "nonsense"
+  class PrayerMateApiException < RuntimeError
+  end
 
-  def api(protocol, host)
-    PrayerMateApi::Api.new protocol, host
+  def api(protocol, host, api_key, session = nil)
+    PrayerMateApi::Api.new protocol, host, api_key, session
   end
 
   class Api
-    attr_accessor :url_prefix
+    attr_accessor :url_prefix, :api_key, :session_token, :email
 
-    def initialize(protocol, host)
+    def initialize(protocol, host, api_key, session = nil)
       self.url_prefix = "#{protocol}://#{host}/api"
+      self.api_key = api_key
+
+      if session
+        self.session_token = session[:api_token]
+        self.email = session[:email]
+      end
     end
 
     def resize_image(url, filename)
-      api_path = "#{self.url_prefix}/images/resize"
-      HTTParty.post(api_path, :body => { :url => url, :target_filename => filename }, :headers => http_headers)
+      HTTParty.post(api_path("images/resize"), body: { url: url, target_filename: filename }, headers: http_headers)
+    end
+
+    def signup(promo_code, first_name, last_name, email, roles)
+      data = {
+          :promo_code => promo_code,
+          :first_name => first_name,
+          :last_name => last_name,
+          :email => email,
+          :password => SecureRandom.hex(8),
+          :roles => roles
+      }
+
+      response = post_with_auth("auth/register", data)
+      response[:password] = data[:password]
+      response
     end
 
     def update_input_feed(feed_id, petitions, last_modified)
-      api_path = "#{self.url_prefix}/input_feeds/process"
-      HTTParty.post(api_path, :body => { :feed => { :id => feed_id, :last_modified => last_modified }, :petitions => petitions }, :headers => http_headers)
+      HTTParty.post(api_path("input_feeds/process"), body: { feed: { id: feed_id, last_modified: last_modified }, petitions: petitions }, headers: http_headers)
+    end
+
+    def update_feed(feed_id, petitions, last_modified, url)
+      HTTParty.post(api_path("feeds/process"), body: { feed: { id: feed_id, last_modified: last_modified, static_json_url: url }, petitions: petitions }, headers: http_headers)
+    end
+
+    def post_with_auth(endpoint, data)
+      response = HTTParty.post(api_path(endpoint), body: data, headers: http_headers, basic_auth: auth_hash)
+      process_response response
+    end
+
+    def get_with_auth(endpoint)
+      response = HTTParty.get(api_path(endpoint), headers: http_headers, basic_auth: auth_hash)
+      process_response response
     end
 
     protected
 
+    def api_path(endpoint)
+      "#{self.url_prefix}/#{endpoint}"
+    end
+
+    def process_response(response)
+      begin
+        body = JSON.parse(response.body)
+      rescue => error
+        body = { errors: error.to_s }
+      end
+      return body if response.code == 200
+
+      raise PrayerMateApiException.new("Status code #{response.code}: #{body[:errors]}")
+    end
+
+    def auth_hash
+      { username: email, password: session_token }
+    end
+
     def http_headers
-      { "X-API-Key" => API_KEY }
+      { "X-API-Key" => api_key }
     end
   end
 end
